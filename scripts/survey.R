b3d16cc04cf3dd2592ca4ea6e7979505b0ed48b6
kaiwenjanet <kjin7@student.monash.edu>
2020-11-27 12:10:42 +0800

    updated

--- scripts/survey.R
+++ scripts/survey.R
@@ -30,110 +30,6 @@ survey_df %>% group_by(eco) %>% dplyr::summarise(n = n()/12)
 
 count(survey_df$identifier)
 
-result <- survey_df %>%  group_by(plot_name) %>%  
-  # filter(responses == (correct_response[plot_name]))
-  dplyr::summarise(n = n(), correct = sum(responses==as.numeric(correct_response[plot_name])))
-
-# Marginal residuals
-## V1
-result %>% filter(str_detect(plot_name, '1._1')) %>% summarise(sum(n), sum(correct))
-## V2
-result %>% filter(str_detect(plot_name, '2._1')) %>% summarise(sum(n), sum(correct))
-## V3
-result %>% filter(str_detect(plot_name, '3._1')) %>% summarise(sum(n), sum(correct))
-
-# Conditional residuals
-## V1
-result %>% filter(str_detect(plot_name, '1._2')) %>% summarise(sum(n), sum(correct))
-## V2
-result %>% filter(str_detect(plot_name, '2._2')) %>% summarise(sum(n), sum(correct))
-## V3
-result %>% filter(str_detect(plot_name, '3._2')) %>% summarise(sum(n), sum(correct))
-
-# Conditional residuals Normality
-## V1
-result %>% filter(str_detect(plot_name, '1._3')) %>% summarise(sum(n), sum(correct))
-## V2
-result %>% filter(str_detect(plot_name, '2._3')) %>% summarise(sum(n), sum(correct))
-## V3
-result %>% filter(str_detect(plot_name, '3._3')) %>% summarise(sum(n), sum(correct))
-
-# Least confounded residual normality
-## V1
-result %>% filter(str_detect(plot_name, '1._4')) %>% summarise(sum(n), sum(correct))
-## V2
-result %>% filter(str_detect(plot_name, '2._4')) %>% summarise(sum(n), sum(correct))
-## V3
-result %>% filter(str_detect(plot_name, '3._4')) %>% summarise(sum(n), sum(correct))
-
-## Aut
-result %>% filter(str_detect(plot_name, 'aut_.3._1')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'aut_.3._2')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'aut_.3._3')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'aut_.3._4')) %>% summarise(sum(n), sum(correct))
-
-# Lin
-result %>% filter(str_detect(plot_name, 'lin_.3._1')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'lin_.3._2')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'lin_.3._3')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'lin_.3._4')) %>% summarise(sum(n), sum(correct))
-
-# slp
-result %>% filter(str_detect(plot_name, 'slp_.3._1')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'slp_.3._2')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'slp_.3._3')) %>% summarise(sum(n), sum(correct))
-result %>% filter(str_detect(plot_name, 'slp_.3._4')) %>% summarise(sum(n), sum(correct))
-
-
-# Certainty
-certainty <- survey_df %>%
-  mutate(detect = ifelse(responses == correct_response[plot_name], 1, 0)) %>% 
-  mutate(detect_f = factor(detect, levels = c(0, 1), 
-                           labels = c("Detected? No", "Detected? Yes"))) %>% 
-  mutate(Detected = ifelse(detect_f == "Detected? Yes", "Yes", "No")) %>% 
-  mutate(vers = case_when(
-    str_detect(plot_name, 'v1') ~ "Version 1",
-    str_detect(plot_name, 'v2') ~ "Version 2",
-    str_detect(plot_name, 'v3') ~ "Version 3"
-  )) %>% 
-  mutate(type = case_when(
-    str_detect(plot_name, 'aut') ~ "Mixed",
-    str_detect(plot_name, 'lin') ~ "Categorical",
-    str_detect(plot_name, 'slp') ~ "Numerical"
-  )) %>% 
-  group_by(vers, type) %>%
-  ggplot(aes(certainty, fill = Detected)) + geom_bar() + 
-  facet_grid(type ~ vers) +
-  theme(legend.position = "bottom") + 
-  xlab("Level of certainty")
-
-ggsave(filename = 
-         "~/Desktop/Masters/Master_Econometrics/S2 2020/ETF5550/master/thesis/figures/certainty.png", 
-       plot = certainty, device = "png", dpi = 300,
-       height = 12, width = 12)
-
-## v1
-certainty %>% filter(str_detect(plot_name, 'v1')) %>% 
-  group_by(certainty) %>% ggplot(aes(certainty)) + geom_bar()
-  #group_by(certainty) %>% tally()
-
-## v2
-certainty %>% filter(str_detect(plot_name, 'v2')) %>% 
-  group_by(certainty) %>% ggplot(aes(certainty)) + geom_bar()
-
-## v3
-certainty %>% filter(str_detect(plot_name, 'v3')) %>% 
-  group_by(certainty) %>% ggplot(aes(certainty)) + geom_bar()
-h <- certainty %>% filter(str_detect(plot_name, 'v1')) %>% 
-  group_by(certainty) %>% dplyr::summarise(n = n()) %>% 
-  ggplot(aes(certainty)) + geom_bar()
-
-certainty %>% filter(str_detect(plot_name, 'v2')) %>% 
-  group_by(certainty) %>% tally()
-
-certainty %>% filter(str_detect(plot_name, 'v3')) %>% 
-  group_by(certainty) %>% tally()
-
 correct_response <- c("aut_v11_1.png" = 7, "aut_v12_1.png" = 7, 
                       "aut_v13_1.png" = 13, "aut_v14_1.png" = 13,
                       "lin_v11_1.png" = 7, "lin_v12_1.png" = 7,
@@ -218,201 +114,99 @@ correct_response <- c("aut_v11_1.png" = 7, "aut_v12_1.png" = 7,
                       "slp_v31_4.png" = 11, "slp_v32_4.png" = 11,
                       "slp_v33_4.png" = 6, "slp_v34_4.png" = 6)
 
-#V11
-survey_df[survey_df$plot_name == "aut_v11_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v12_1.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v13_1.png",]$responses # 5
-survey_df[survey_df$plot_name == "aut_v14_1.png",]$responses # 2 // 8
 
-survey_df[survey_df$plot_name == "lin_v11_1.png",]$responses # 2 responses
-survey_df[survey_df$plot_name == "lin_v12_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v13_1.png",]$responses # 3 
-survey_df[survey_df$plot_name == "lin_v14_1.png",]$responses # 1 // 7
+result <- survey_df %>%  group_by(plot_name) %>%  
+  # filter(responses == (correct_response[plot_name]))
+  dplyr::summarise(n = n(), correct = sum(responses==as.numeric(correct_response[plot_name])))
 
-survey_df[survey_df$plot_name == "slp_v11_1.png",]$responses # 0
-survey_df[survey_df$plot_name == "slp_v12_1.png",]$responses # 2 
-survey_df[survey_df$plot_name == "slp_v13_1.png",]$responses # 4
-survey_df[survey_df$plot_name == "slp_v14_1.png",]$responses # 3 // 9
+# Marginal residuals
+## V1
+result %>% filter(str_detect(plot_name, '1._1')) %>% summarise(sum(n), sum(correct))
+## V2
+result %>% filter(str_detect(plot_name, '2._1')) %>% summarise(sum(n), sum(correct))
+## V3
+result %>% filter(str_detect(plot_name, '3._1')) %>% summarise(sum(n), sum(correct))
 
-#V12
-survey_df[survey_df$plot_name == "aut_v11_2.png",]$responses # 2
-survey_df[survey_df$plot_name == "aut_v12_2.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v13_2.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v14_2.png",]$responses # 3 // 6
+# Conditional residuals
+## V1
+result %>% filter(str_detect(plot_name, '1._2')) %>% summarise(sum(n), sum(correct))
+## V2
+result %>% filter(str_detect(plot_name, '2._2')) %>% summarise(sum(n), sum(correct))
+## V3
+result %>% filter(str_detect(plot_name, '3._2')) %>% summarise(sum(n), sum(correct))
 
-survey_df[survey_df$plot_name == "lin_v11_2.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v12_2.png",]$responses # 3 
-survey_df[survey_df$plot_name == "lin_v13_2.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v14_2.png",]$responses # 2 // 5
+# Conditional residuals Normality
+## V1
+result %>% filter(str_detect(plot_name, '1._3')) %>% summarise(sum(n), sum(correct))
+## V2
+result %>% filter(str_detect(plot_name, '2._3')) %>% summarise(sum(n), sum(correct))
+## V3
+result %>% filter(str_detect(plot_name, '3._3')) %>% summarise(sum(n), sum(correct))
 
-survey_df[survey_df$plot_name == "slp_v11_2.png",]$responses # 2
-survey_df[survey_df$plot_name == "slp_v12_2.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v13_2.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v14_2.png",]$responses # 1 // 5
+# Least confounded residual normality
+## V1
+result %>% filter(str_detect(plot_name, '1._4')) %>% summarise(sum(n), sum(correct))
+## V2
+result %>% filter(str_detect(plot_name, '2._4')) %>% summarise(sum(n), sum(correct))
+## V3
+result %>% filter(str_detect(plot_name, '3._4')) %>% summarise(sum(n), sum(correct))
 
-#V13
-survey_df[survey_df$plot_name == "aut_v11_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v12_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "aut_v13_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v14_3.png",]$responses # 3 // 5
+## Aut
+result %>% filter(str_detect(plot_name, 'aut_.3._1')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'aut_.3._2')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'aut_.3._3')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'aut_.3._4')) %>% summarise(sum(n), sum(correct))
 
-survey_df[survey_df$plot_name == "lin_v11_3.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v12_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v13_3.png",]$responses # 4
-survey_df[survey_df$plot_name == "lin_v14_3.png",]$responses # 3 // 10
+# Lin
+result %>% filter(str_detect(plot_name, 'lin_.3._1')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'lin_.3._2')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'lin_.3._3')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'lin_.3._4')) %>% summarise(sum(n), sum(correct))
 
-survey_df[survey_df$plot_name == "slp_v11_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "slp_v12_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "slp_v13_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "slp_v14_3.png",]$responses # 0 // 4
+# slp
+result %>% filter(str_detect(plot_name, 'slp_.3._1')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'slp_.3._2')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'slp_.3._3')) %>% summarise(sum(n), sum(correct))
+result %>% filter(str_detect(plot_name, 'slp_.3._4')) %>% summarise(sum(n), sum(correct))
 
-#V14
-survey_df[survey_df$plot_name == "aut_v11_4.png",]$responses # 3
-survey_df[survey_df$plot_name == "aut_v12_4.png",]$responses # 2
-survey_df[survey_df$plot_name == "aut_v13_4.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v14_4.png",]$responses # 2 // 7
 
-survey_df[survey_df$plot_name == "lin_v11_4.png",]$responses # 5
-survey_df[survey_df$plot_name == "lin_v12_4.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v13_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v14_4.png",]$responses # 0 // 6
+# Certainty
+certainty <- survey_df %>%
+  mutate(detect = ifelse(responses == correct_response[plot_name], 1, 0)) %>% 
+  mutate(detect_f = factor(detect, levels = c(0, 1), 
+                           labels = c("Detected? No", "Detected? Yes"))) %>% 
+  mutate(Detected = ifelse(detect_f == "Detected? Yes", "Yes", "No")) %>% 
+  mutate(vers = case_when(
+    str_detect(plot_name, 'v1') ~ "Version 1",
+    str_detect(plot_name, 'v2') ~ "Version 2",
+    str_detect(plot_name, 'v3') ~ "Version 3"
+  )) %>% 
+  mutate(type = case_when(
+    str_detect(plot_name, 'aut') ~ "Mixed",
+    str_detect(plot_name, 'lin') ~ "Categorical",
+    str_detect(plot_name, 'slp') ~ "Numerical"
+  )) %>% 
+  group_by(vers, type) %>%
+  ggplot(aes(certainty, fill = Detected)) + geom_bar() + 
+  facet_grid(type ~ vers) +
+  theme(legend.position = "bottom") + 
+  xlab("Level of certainty")
 
-survey_df[survey_df$plot_name == "slp_v11_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v12_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v13_4.png",]$responses # 2 # correct 1
-survey_df[survey_df$plot_name == "slp_v14_4.png",]$responses # 2 // 4
 
-# For Version 1, 1/76
+survey_df %>% filter(str_detect(plot_name, "v3")) %>% tally() #223
+survey_df %>% filter(str_detect(plot_name, "v2")) %>% tally() #194
+survey_df %>% filter(str_detect(plot_name, "v1")) %>% tally() #183
 
-#V21
-survey_df[survey_df$plot_name == "aut_v21_1.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v22_1.png",]$responses # 3 # correct 1
-survey_df[survey_df$plot_name == "aut_v23_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v24_1.png",]$responses # 1 // 5
-
-survey_df[survey_df$plot_name == "lin_v21_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v22_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v23_1.png",]$responses # 2 
-survey_df[survey_df$plot_name == "lin_v24_1.png",]$responses # 0 // 4
-
-survey_df[survey_df$plot_name == "slp_v21_1.png",]$responses # 0
-survey_df[survey_df$plot_name == "slp_v22_1.png",]$responses # 2 
-survey_df[survey_df$plot_name == "slp_v23_1.png",]$responses # 0
-survey_df[survey_df$plot_name == "slp_v24_1.png",]$responses # 1 // 3
-
-#V22
-survey_df[survey_df$plot_name == "aut_v21_2.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v22_2.png",]$responses # 2
-survey_df[survey_df$plot_name == "aut_v23_2.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v24_2.png",]$responses # 2 // 5
-
-survey_df[survey_df$plot_name == "lin_v21_2.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v22_2.png",]$responses # 2 
-survey_df[survey_df$plot_name == "lin_v23_2.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v24_2.png",]$responses # 2 // 7
-
-survey_df[survey_df$plot_name == "slp_v21_2.png",]$responses # 2 # correct 1
-survey_df[survey_df$plot_name == "slp_v22_2.png",]$responses # 0
-survey_df[survey_df$plot_name == "slp_v23_2.png",]$responses # 1 
-survey_df[survey_df$plot_name == "slp_v24_2.png",]$responses # 1 // 4
-
-#V23
-survey_df[survey_df$plot_name == "aut_v21_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v22_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v23_3.png",]$responses # 3
-survey_df[survey_df$plot_name == "aut_v24_3.png",]$responses # 0 // 3
-
-survey_df[survey_df$plot_name == "lin_v21_3.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v22_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v23_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v24_3.png",]$responses # 1 // 6
-
-survey_df[survey_df$plot_name == "slp_v21_3.png",]$responses # 3
-survey_df[survey_df$plot_name == "slp_v22_3.png",]$responses # 3
-survey_df[survey_df$plot_name == "slp_v23_3.png",]$responses # 2 
-survey_df[survey_df$plot_name == "slp_v24_3.png",]$responses # 1 // 9
-
-#V24
-survey_df[survey_df$plot_name == "aut_v21_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v22_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v23_4.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v24_4.png",]$responses # 0 // 2
-
-survey_df[survey_df$plot_name == "lin_v21_4.png",]$responses # 3
-survey_df[survey_df$plot_name == "lin_v22_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v23_4.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v24_4.png",]$responses # 3 # correct 1 // 7
-
-survey_df[survey_df$plot_name == "slp_v21_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v22_4.png",]$responses # 2
-survey_df[survey_df$plot_name == "slp_v23_4.png",]$responses # 1 
-survey_df[survey_df$plot_name == "slp_v24_4.png",]$responses # 1 // 4
-
-# For Version2, 3/59
-
-#V31
-survey_df[survey_df$plot_name == "aut_v31_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "aut_v32_1.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v33_1.png",]$responses # 3
-survey_df[survey_df$plot_name == "aut_v34_1.png",]$responses # 1 // 5
-
-survey_df[survey_df$plot_name == "lin_v31_1.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v32_1.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v33_1.png",]$responses # 1 
-survey_df[survey_df$plot_name == "lin_v34_1.png",]$responses # 2 // 4
-
-survey_df[survey_df$plot_name == "slp_v31_1.png",]$responses # 2 # correct 1
-survey_df[survey_df$plot_name == "slp_v32_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v33_1.png",]$responses # 1
-survey_df[survey_df$plot_name == "slp_v34_1.png",]$responses # 2 // 6
-
-#V32
-survey_df[survey_df$plot_name == "aut_v31_2.png",]$responses # 1 # correct 1
-survey_df[survey_df$plot_name == "aut_v32_2.png",]$responses # 3 # correct 3
-survey_df[survey_df$plot_name == "aut_v33_2.png",]$responses # 2 # correct 2
-survey_df[survey_df$plot_name == "aut_v34_2.png",]$responses # 1 // 7 # correct 1
-
-survey_df[survey_df$plot_name == "lin_v31_2.png",]$responses # 2
-survey_df[survey_df$plot_name == "lin_v32_2.png",]$responses # 1 
-survey_df[survey_df$plot_name == "lin_v33_2.png",]$responses # 2 # correct 2
-survey_df[survey_df$plot_name == "lin_v34_2.png",]$responses # 1 // 6 # correct 1
-
-survey_df[survey_df$plot_name == "slp_v31_2.png",]$responses # 2 
-survey_df[survey_df$plot_name == "slp_v32_2.png",]$responses # 3
-survey_df[survey_df$plot_name == "slp_v33_2.png",]$responses # 3 
-survey_df[survey_df$plot_name == "slp_v34_2.png",]$responses # 1 // 9
-
-#V33
-survey_df[survey_df$plot_name == "aut_v31_3.png",]$responses # 4 # all c
-survey_df[survey_df$plot_name == "aut_v32_3.png",]$responses # 4 # all C
-survey_df[survey_df$plot_name == "aut_v33_3.png",]$responses # 2
-survey_df[survey_df$plot_name == "aut_v34_3.png",]$responses # 0 // 10
-
-survey_df[survey_df$plot_name == "lin_v31_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v32_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v33_3.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v34_3.png",]$responses # 2 // 2
-
-survey_df[survey_df$plot_name == "slp_v31_3.png",]$responses # 1 # AC
-survey_df[survey_df$plot_name == "slp_v32_3.png",]$responses # 1 # AC
-survey_df[survey_df$plot_name == "slp_v33_3.png",]$responses # 0 
-survey_df[survey_df$plot_name == "slp_v34_3.png",]$responses # 3 // 5 # C2
-
-#V34
-survey_df[survey_df$plot_name == "aut_v31_4.png",]$responses # 5
-survey_df[survey_df$plot_name == "aut_v32_4.png",]$responses # 0
-survey_df[survey_df$plot_name == "aut_v33_4.png",]$responses # 1 #AC
-survey_df[survey_df$plot_name == "aut_v34_4.png",]$responses # 3 // 9 #C1
-
-survey_df[survey_df$plot_name == "lin_v31_4.png",]$responses # 0
-survey_df[survey_df$plot_name == "lin_v32_4.png",]$responses # 3
-survey_df[survey_df$plot_name == "lin_v33_4.png",]$responses # 1
-survey_df[survey_df$plot_name == "lin_v34_4.png",]$responses # 1 // 5
-
-survey_df[survey_df$plot_name == "slp_v31_4.png",]$responses # 2
-survey_df[survey_df$plot_name == "slp_v32_4.png",]$responses # 2
-survey_df[survey_df$plot_name == "slp_v33_4.png",]$responses # 1 
-survey_df[survey_df$plot_name == "slp_v34_4.png",]$responses # 2 // 7
-
-#For Version 3, 25/74
-
+version <- survey_df %>% 
+  mutate(detect = ifelse(responses == correct_response[plot_name], 1, 0)) %>% 
+  mutate(detect_f = factor(detect, levels = c(0, 1), 
+                           labels = c("Detected? No", "Detected? Yes"))) %>% 
+  mutate(Detected = ifelse(detect_f == "Detected? Yes", "Yes", "No")) %>% 
+  mutate(vers = case_when(
+  str_detect(plot_name, 'v1') ~ "Version 1",
+  str_detect(plot_name, 'v2') ~ "Version 2",
+  str_detect(plot_name, 'v3') ~ "Version 3"
+)) %>% 
+  group_by(vers) %>% tally()
+  ggplot(aes(vers, fill = Detected)) + geom_bar() + theme(legend.position = "bottom") + 
+  xlab("Version")
